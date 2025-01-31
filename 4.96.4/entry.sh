e626c368408922703a47dd8325e3a8075340a7aa
Tyson Swetnam <tswetnam@cyverse.org>
2025-01-31 13:07:11 -0700

    created 4.96.4

---
+++ 4.96.4/entry.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Ensure .irods directory and environment file exist
+mkdir -p /home/vscode/.irods
+cat <<EOF > /home/vscode/.irods/irods_environment.json
+{
+  "irods_host": "data.cyverse.org",
+  "irods_port": 1247,
+  "irods_user_name": "$IPLANT_USER",
+  "irods_zone_name": "iplant"
+}
+EOF
+
+# Add conda to PATH
+# echo "export PATH=$PATH:/opt/conda/bin" >> /home/vscode/.bashrc
+
+# Copy user configurations if they exist
+if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
+  cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/ 
+fi
+
+if [ -d /data-store/iplant/home/$IPLANT_USER/.ssh ]; then
+  cp -r /data-store/iplant/home/$IPLANT_USER/.ssh ~/ 
+fi
+
+# Start code-server
+exec /app/code-server/bin/code-server --host 0.0.0.0 --disable-telemetry --
