e6d78bcead1ae2b27299b454704350b5d83ae539
Tyson Swetnam <tswetnam@cyverse.org>
2025-04-29 13:40:36 -0700

    updated latest

--- latest/entry.sh
+++ latest/entry.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
+# Ensure .irods directory and environment file exist
+mkdir -p /home/vscode/.irods
+cat <<EOF > /home/vscode/.irods/irods_environment.json
+{
+  "irods_host": "data.cyverse.org",
+  "irods_port": 1247,
+  "irods_user_name": "$IPLANT_USER",
+  "irods_zone_name": "iplant"
+}
+EOF
 
-exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --without-connection-token "${@}" --
+# Add conda to PATH
+# echo "export PATH=$PATH:/opt/conda/bin" >> /home/vscode/.bashrc
+
+# Copy user configurations if they exist
+if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
+  cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/ 
+fi
+
+if [ -d /data-store/iplant/home/$IPLANT_USER/.ssh ]; then
+  cp -r /data-store/iplant/home/$IPLANT_USER/.ssh ~/ 
+fi
+
+PASSWORD=password
+
+# Start code-server
+exec /app/code-server/bin/code-server --host 0.0.0.0  --cert --bind-addr 0.0.0.0:8080 /app --disable-telemetry --
