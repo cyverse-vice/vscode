7305cc5db7833021e90b3131daba817b353a3832
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2025-09-19 07:04:35 -0700

    Refactor entry.sh for irods and mcp server setup
    Removed old irods configuration setup and added new configuration for irods and mcp server.

--- latest/entry.sh
+++ latest/entry.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
+# add irods config
+echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
+# add mcp server
+echo '{"mcpServers":{"filesystem":{"command":"npx","args":["-y","@modelcontextprotocol/server-filesystem","/home/jovyan/work","/home/jovyan/data","/home/jovyan/notebooks","/home/jovyan"],"env":{}}}}' > /home/jovyan/.claude.json
 
-# Ensure .irods directory and environment file exist
-mkdir -p /home/vscode/.irods
-cat <<EOF > /home/vscode/.irods/irods_environment.json
-{
-  "irods_host": "data.cyverse.org",
-  "irods_port": 1247,
-  "irods_user_name": "$IPLANT_USER",
-  "irods_zone_name": "iplant"
-}
-EOF
-
-# Add conda to PATH
-# echo "export PATH=$PATH:/opt/conda/bin" >> /home/vscode/.bashrc
-
-# Copy user configurations if they exist
+# Copy .gitconfig from volume mount (if it exists)
 if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
   cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/ 
 fi
 
+# Copy S3 AWS (if it exists)
 if [ -d /data-store/iplant/home/$IPLANT_USER/.aws ]; then
   cp -r /data-store/iplant/home/$IPLANT_USER/.aws ~/ 
 fi
 
+# Copy SSH keys (if it exists)
 if [ -d /data-store/iplant/home/$IPLANT_USER/.ssh ]; then
   cp -r /data-store/iplant/home/$IPLANT_USER/.ssh ~/ 
 fi
+
 
 if [ -d /data-store/iplant/home/$IPLANT_USER/.vscode-server ]; then
   cp -r /data-store/iplant/home/$IPLANT_USER/.vscode-server ~/ 
